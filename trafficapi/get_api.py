b03055c9b3d064f1a7b712534856ae38dd25b49a
Kevin_laptop <kevin820606@gmail.com>
2021-10-18 19:15:19 +0800

    little modify

--- trafficapi/get_api.py
+++ trafficapi/get_api.py
@@ -1,15 +1,21 @@
 import requests
-from requests.exceptions import HTTPError
+from utils import trafficapidata
 
 
 class APIquery:
     def __init__(self) -> None:
         self.url = "https://od.moi.gov.tw/MOI/v1/pbs/"
+        self.data: dict[str, trafficapidata] = dict()
 
-    def _get_data(self) -> list[dict]:
+    def __get_data(self) -> list[dict]:
 
         req = requests.get(self.url)
         return req.json()
 
-    def _data_parse(self):
-        self.json = self._get_data()
+    def __data_parse(self):
+        apidata = self.__get_data()
+        for apidatum in apidata:
+            self.data |= {apidatum["UID"]: trafficapidata(**apidatum)}
+
+    def run(self):
+        self.__data_parse()
